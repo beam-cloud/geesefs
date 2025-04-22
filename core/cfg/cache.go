a0544e1f7dde3688b993ecda5f23ee206b3f6c04
Luke Lombardi <luke@beam.cloud>
2025-04-21 20:17:14 -0400

    fix path

--- core/cfg/cache.go
+++ core/cfg/cache.go
@@ -4,14 +4,15 @@ type ContentCache interface {
 	GetContent(hash string, offset int64, length int64) ([]byte, error)
 	StoreContent(chunks chan []byte, hash string) (string, error)
 	StoreContentFromSource(source struct {
+		Path        string
 		BucketName  string
-		Key         string
 		Region      string
 		EndpointURL string
 		AccessKey   string
 		SecretKey   string
 	}) (string, error)
 	StoreContentFromSourceWithLock(source struct {
+		Path        string
 		BucketName  string
 		Key         string
 		Region      string
